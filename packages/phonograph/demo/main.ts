22fc4664679d27645d87910b6645c6c87c318c00
LOSSES Don <1384036+Losses@users.noreply.github.com>
2023-07-09 19:47:52 +0800

    docs(phonograph): Update phonograph content

--- packages/phonograph/demo/main.ts
+++ packages/phonograph/demo/main.ts
@@ -1,39 +1,47 @@
-import { Clip, mp3Adapter } from '../src/index';
+import { Clip, Mp3DeMuxAdapter } from '../src/index';
 
 const clip = new Clip({
   url: '/demo/deepnote.mp3',
-  adapter: mp3Adapter,
+  adapter: new Mp3DeMuxAdapter(),
 });
 
-clip.buffer().then(() => {
-  const play = document.querySelector('#play') as HTMLButtonElement;
-  const pause = document.querySelector('#pause') as HTMLButtonElement;
-  const progress = document.querySelector('#progress') as HTMLSpanElement;
+const init = document.querySelector('#init') as HTMLButtonElement;
 
-  play.disabled = false;
-  pause.disabled = false;
+init.addEventListener('click', () => {
+  clip.buffer().then(() => {
+    const play = document.querySelector('#play') as HTMLButtonElement;
+    const pause = document.querySelector('#pause') as HTMLButtonElement;
+    const progress = document.querySelector('#progress') as HTMLSpanElement;
 
-  play.addEventListener('click', () => {
-    clip.play();
+    init.disabled = true;
+    play.disabled = false;
+    pause.disabled = false;
+
+    play.addEventListener('click', () => {
+      clip.play();
+    });
+
+    pause.addEventListener('click', () => {
+      clip.pause();
+    });
+
+    const updateProgress = () => {
+      progress.innerText = `${clip.currentTime}`;
+    };
+
+    console.log(clip);
+    clip.on('play', updateProgress);
+    clip.on('pause', updateProgress);
+    clip.on('ended', updateProgress);
+
+    const loop = () => {
+      updateProgress();
+      requestAnimationFrame(loop);
+    };
+    loop();
   });
-
-  pause.addEventListener('click', () => {
-    clip.pause();
-  });
-
-  const updateProgress=()=>{
-    progress.innerText=`${clip.currentTime}`
-  }
-
-  clip.on('play',updateProgress)
-  clip.on('pause',updateProgress)
-  clip.on('ended',updateProgress)
-
-  const loop=()=>{
-    updateProgress()
-    requestAnimationFrame(loop)
-  }
-  loop()
 });
 
-(window as any).clip=clip;
+// For debug purpose
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).clip = clip;
