f68f605aeabf0870179533922fab4d083ca2a503
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-05-09 16:29:38 +0800

    fix(phonograph): Header is not correctly parsed

--- packages/phonograph/src/adapters/mp3/Mp3DeMuxAdapter.ts
+++ packages/phonograph/src/adapters/mp3/Mp3DeMuxAdapter.ts
@@ -162,7 +162,7 @@ export class Mp3DeMuxAdapter extends MediaDeMuxAdapter<
       // Sampling Rate Frequency should not be 11, the value is reserved
       (data[i + 2] & 0b00001100) !== 0b00001100 &&
       // Emphasis should not be 10, the value is reserved
-      (data[i + 2] & 0b00000011) !== 0b00000010;
+      (data[i + 3] & 0b00000011) !== 0b00000010;
 
     return valid;
   };
