12dd84f08a4b54e16c6a0b23cca94893caebaa0b
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-07-04 19:54:54 +0800

    feat(ogg-polyfill): Better OGG Vorbis example

--- packages/ogg-polyfill/example/parseOggVorbisFile.ts
+++ packages/ogg-polyfill/example/parseOggVorbisFile.ts
@@ -1,23 +1,63 @@
+/* eslint-disable no-console */
 import {
   IOggVorbisPage,
   fetchOggVorbisFile,
+  readOggVorbisFile,
 } from '../src/fetchOggVorbisFile';
 import { oggVorbisExample } from './@example';
 
-(async () => {
-  const x = fetchOggVorbisFile(oggVorbisExample, true, 4);
+document.addEventListener("DOMContentLoaded", () => {
+  const $fileInput = document.querySelector('input[type="file"]') as HTMLInputElement | null;
+  const $form = document.querySelector('form') as HTMLFormElement | null;
+  const $urlInput = $form?.querySelector('input[name="url"]') as HTMLInputElement | null;
 
-  let done = false;
-  let data:
-    | IOggVorbisPage
-    | null = null;
+  if (!$fileInput) throw new Error(`fileInput not found`);
+  if (!$form) throw new Error(`form not found`);
+  if (!$urlInput) throw new Error(`urlInput not found`);
 
-  while (!done) {
-    const { done: d, value: v } = await x.next();
+  $urlInput.value = oggVorbisExample;
 
-    done = !!d;
-    data = v;
+  $fileInput.addEventListener('change', async (event) => {
+    console.clear();
 
-    console.log(data);
-  }
-})();
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const reader = (file.stream() as unknown as ReadableStream<Uint8Array>).getReader();
+      const oggVorbisReader = readOggVorbisFile(reader, true, 4);
+
+      let done = false;
+      let data: IOggVorbisPage | null = null;
+
+      while (!done) {
+        const { done: d, value: v } = await oggVorbisReader.next();
+
+        done = !!d;
+        data = v;
+
+        console.log(data);
+      }
+    }
+  });
+
+  $form.addEventListener('submit', async (event) => {
+    console.clear();
+
+    event.preventDefault();
+    const url = $urlInput?.value;
+    if (url) {
+      const x = fetchOggVorbisFile(url, true, 4);
+
+      let done = false;
+      let data: IOggVorbisPage | null = null;
+
+      while (!done) {
+        const { done: d, value: v } = await x.next();
+
+        done = !!d;
+        data = v;
+
+        console.log(data);
+      }
+    }
+  });
+});
